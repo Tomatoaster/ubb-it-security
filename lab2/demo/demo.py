8011aaaf378f91076f28df179d2220ff59d4f7ec
Tomatoaster <lszltamas94@gmail.com>
2025-03-31 22:22:41 +0300

    mypy demo

--- lab2/demo/demo.py
+++ lab2/demo/demo.py
@@ -2,24 +2,28 @@
 
 class BankAccount:
     
-    def __init__(self, owner, account_number, initial_balance = 0):
+    def __init__(self,
+                 owner: str,
+                 account_number: tuple[int, int, int],
+                 initial_balance: int = 0) -> None:
         self.owner = owner
         self.account_number = account_number
         self.balance = initial_balance
     
-    def deposit(self, amount):
+    def deposit(self, amount: int) -> None:
         self.balance += amount
     
-    def withdraw(self, amount):
+    def withdraw(self, amount: int) -> None:
         self.balance -= amount
     
-    def is_overdrawn(self):
+    def is_overdrawn(self) -> bool:
         return self.balance < 0
     
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Balance of {self.owner} (account number: {self.account_number}) is: {self.balance}"
 
 
 account = BankAccount("Dagobert Duck", (12345678,87654321,00000000), 15)
 account.withdraw(5)
+# account.deposit("test")
 print(account)
